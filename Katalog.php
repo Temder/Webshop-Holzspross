506855d901f0fc145e77845265403bef3a208a7c
Leon2301 <57454383+Leon2301@users.noreply.github.com>
2020-09-05 14:35:02 +0200

    Update Katalog.php

---
+++ Katalog.php
@@ -0,0 +1,24 @@
+<!-- 
+Das ist die Seite "Katalog" 
+von Leon Mantey. 
+-->
+
+<?php
+error_reporting(-1); 
+ini_set('display_errors','On');
+?>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Page Title</title>
+    </head>
+
+    <body>
+    
+        <h1>Holzspross-Katalog</h1>
+        <p>Hier gibt es unsere Produkte aus Holz.</p>
+
+    </body>
+</html>
