4c09f24482bd54088606238c375e3ee875910009
Leon2301 <57454383+Leon2301@users.noreply.github.com>
2020-09-05 14:04:42 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+error_reporting(-1);
+ini_set('display_errors','On');
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <title>Holzspross</title>
+        <meta charset="Utf-8">
+        <link type="text/css" href="Assets/css/bootstrap.min.css">
+    </head>
+
+    <body>
+        <H1>Holzspross</H1>
+        Hallöchen!
+
+        <script src="Assets/css/bootstrap.min.css"></script>
+    </body>
+</html>
