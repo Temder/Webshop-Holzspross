2e28534b419c7e8c9232b2e50c1080527c2cbafc
Temder <tom.ender03@googlemail.com>
2020-09-05 14:40:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,17 +5,22 @@ ini_set('display_errors','On');
 
 <!DOCTYPE html>
 <html lang="de">
-    <head>
-        <title>Holzspross</title>
-        <link rel="shortcut icon" href="Bilder\cross.ico" />
-        <meta charset="Utf-8">
-        <link type="text/css" href="Assets/css/bootstrap.min.css">
-    </head>
 
-    <body>
-        <H1>Holzspross</H1>
-        Hallöchen!
+<head>
+    <title>Holzspross</title>
+    <link rel="shortcut icon" href="Bilder\cross.ico" />
+    <meta charset="Utf-8">
+    <link type="text/css" href="Assets/css/bootstrap.min.css">
+</head>
 
-        <script src="Assets/css/bootstrap.min.css"></script>
-    </body>
+<body>
+    <div style="text-align: center;">
+        Holzspross
+    </div>
+    <H1>Holzspross</H1>
+    Hallöchen!
+
+    <script src="Assets/css/bootstrap.min.css"></script>
+</body>
+
 </html>
